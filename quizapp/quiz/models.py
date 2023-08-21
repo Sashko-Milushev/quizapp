af557f148d07831e51ea17083174fd14e04d118e
Sashko <milushev.sashko@gmail.com>
2023-08-21 15:01:40 +0300

    Create QuizCategory model

--- quizapp/quiz/models.py
+++ quizapp/quiz/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class QuizCategory(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
