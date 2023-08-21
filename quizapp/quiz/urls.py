423f6dea0ab6d8e2cae21295a1cf8079460b8c41
Sashko <milushev.sashko@gmail.com>
2023-08-21 15:42:11 +0300

    Create view for start quiz session, including both get and post methods.

--- quizapp/quiz/urls.py
+++ quizapp/quiz/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
+from quizapp.quiz.views import StartQuizSession
 
 app_name = 'quiz'
 
 urlpatterns = (
-
+    path('start-quiz-session/', StartQuizSession.as_view(), name='api start quiz session'),
 )
--- quizapp/quiz/views.py
+++ quizapp/quiz/views.py
@@ -1,1 +1,39 @@
+from rest_framework import status
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
+from quizapp.quiz.models import QuizCategory, QuizSession
+from quizapp.quiz.opentdb_api_service import get_random_questions, get_questions_for_category
+
+
+class StartQuizSession(APIView):
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        category_id = request.query_params.get('category_id')  # Get the category ID from query parameters
+
+        # Get the category instance or return a 404 response if not found
+        category = get_object_or_404(QuizCategory, id=category_id)
+
+        # Fetch questions based on the user's choice
+        if category.name == 'Random':
+            quiz_questions = get_random_questions()
+        else:
+            quiz_questions = get_questions_for_category(category)
+
+        return Response({'questions': quiz_questions}, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        category_id = request.data.get('category_id')
+
+        # Get the category instance or return a 404 response if not found
+        category = get_object_or_404(QuizCategory, id=category_id)
+
+        # Create a new QuizSession instance
+        session = QuizSession.objects.create(user=user, category=category)
+
+        # Calculate and update points for the session
+        session.calculate_points()
+
+        return Response({'points': session.points}, status=status.HTTP_201_CREATED)
